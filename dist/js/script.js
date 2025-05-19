9c55f0c6cd38322e3f247653a039e291600a71b3
Alvien Cenna <alpiencenna@gmail.com>
2025-05-19 09:58:23 +0700

    update: add more portfolios

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,7 @@
-window.onscroll = function() {
+window.onscroll = function () {
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
-    if(window.pageYOffset > fixedNav) {
+    if (window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed');
     } else {
         header.classList.remove('navbar-fixed')
@@ -12,7 +12,29 @@ window.onscroll = function() {
 const hamburger = document.querySelector('#hamburger');
 const navMenu = document.querySelector('#nav-menu');
 
-hamburger.addEventListener('click', function() {
-   hamburger.classList.toggle('hamburger-active');
-   navMenu.classList.toggle('hidden');
+hamburger.addEventListener('click', function () {
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
+});
+
+
+const modal = document.getElementById('modal');
+const modalContent = modal.querySelector('div');
+
+document.getElementById('openModal').addEventListener('click', () => {
+    modal.classList.remove('hidden');
+    modal.classList.add('flex');
+});
+
+document.getElementById('closeModal').addEventListener('click', () => {
+    modal.classList.remove('flex');
+    modal.classList.add('hidden');
+});
+
+// click outside to close
+modal.addEventListener('click', (e) => {
+    if (!modalContent.contains(e.target)) {
+        modal.classList.remove('flex');
+        modal.classList.add('hidden');
+    }
 });
